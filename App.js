bae6a9f4eab4e346c62e59016351dfd6a6395ed1
Francisco <franciscozapi@gmail.com>
2020-11-25 23:23:02 -0300

    ESlint config

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Universal React with Expo</Text>
